8db535d75bfaa19cee3df220af2aeed92f637181
Sergio Gómez <sergio@uco.es>
2019-08-13 19:13:54 +0200

    Installed semantic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Container } from "semantic-ui-react";
+import styled from "styled-components";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FunctionComponent = () => {
+  return <Body>Hello world</Body>;
+};
+
+const Body = styled(Container)`
+  margin-top: 64px;
+`;
 
 export default App;
